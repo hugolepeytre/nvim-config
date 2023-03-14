3a587a98a9e4e891362a9175800db9b4f5f963af
hugo <hugo.lepeytre@virtualtrafficlights.com>
2023-03-14 17:12:12 +0100

    Format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,8 +25,31 @@ local null_ls_sources = {
     null_ls.builtins.formatting.black,
 }
 
+local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+        filter = function(client)
+            -- apply whatever logic you want (in this example, we'll only use null-ls)
+            return client.name == "null-ls"
+        end,
+        bufnr = bufnr,
+    })
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-    sources = null_ls_sources
+    sources = null_ls_sources,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    lsp_formatting(bufnr)
+                end,
+            })
+        end
+    end,
 })
 
 require("mason-null-ls").setup({
