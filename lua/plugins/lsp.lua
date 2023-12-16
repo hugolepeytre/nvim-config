fa71e292340e24219d39236013e57c737143a3a6
hugo <hugo.lepeytre@virtualtrafficlights.com>
2023-12-16 19:59:15 +0100

    Finished registering keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,49 +13,30 @@ require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "rust_analyzer", "pylsp" },
 })
 
--- -- only_local ensures mypy and pylint see imports
--- local null_ls = require("null-ls")
--- local null_ls_sources = {
---     -- Python lint and formatting
---     null_ls.builtins.diagnostics.mypy.with({
---         only_local = ".venv/bin/",
---     }),
---     null_ls.builtins.diagnostics.pylint.with({
---         only_local = ".venv/bin",
---         method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
---     }),
--- }
-
--- Format on save
--- null_ls.setup({
---     sources = null_ls_sources,
--- })
-
 -- Mappings for vim diagnostics
-local opts = { noremap = true, silent = true }
-map("n", "s<leader>d", vim.diagnostic.open_float, opts)
-map("n", "s<leader>wd", vim.diagnostic.setqflist)
-map("n", "sp", vim.diagnostic.goto_prev, opts)
-map("n", "sn", vim.diagnostic.goto_next, opts)
-map("n", "sl", vim.diagnostic.setloclist, opts)
+map("n", "s<leader>d", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Open Float (?)" })
+map("n", "s<leader>wd", vim.diagnostic.setqflist, { noremap = true, silent = true, desc = "Add to quickfix list" })
+map("n", "sp", vim.diagnostic.goto_prev, { noremap = true, silent = true, desc = "Previous diagnostic" })
+map("n", "sn", vim.diagnostic.goto_next, { noremap = true, silent = true, desc = "Next diagnostic" })
+map("n", "sl", vim.diagnostic.setloclist, { noremap = true, silent = true, desc = "Add to loclist" })
 
 -- Mappings for lsp
-map("n", "sD", lspbuf.declaration, opts)
-map("n", "sgd", lspbuf.definition, opts)
-map("n", "sh", lspbuf.hover, opts)
-map("n", "si", lspbuf.implementation, opts)
-map("n", "s<leader>h", lspbuf.signature_help, opts)
-map("n", "s<leader>ws", lspbuf.workspace_symbol, opts)
-map("n", "s+", lspbuf.add_workspace_folder, opts)
-map("n", "s-", lspbuf.remove_workspace_folder, opts)
-map("n", "sf", lspbuf.format, opts)
+map("n", "sD", lspbuf.declaration, { noremap = true, silent = true, desc = "Go to declaration" })
+map("n", "sgd", lspbuf.definition, { noremap = true, silent = true, desc = "Go to definition" })
+map("n", "sh", lspbuf.hover, { noremap = true, silent = true, desc = "Hover" })
+map("n", "si", lspbuf.implementation, { noremap = true, silent = true, desc = "Go to implementation" })
+map("n", "s<leader>h", lspbuf.signature_help, { noremap = true, silent = true, desc = "Signature help" })
+map("n", "s<leader>ws", lspbuf.workspace_symbol, { noremap = true, silent = true, desc = "Workspace symbol (?)" })
+map("n", "s+", lspbuf.add_workspace_folder, { noremap = true, silent = true, desc = "Add workspace folder (?)" })
+map("n", "s-", lspbuf.remove_workspace_folder, { noremap = true, silent = true, desc = "Remove workspace folder (?)" })
+map("n", "sf", lspbuf.format, { noremap = true, silent = true, desc = "Format" })
 map("n", "s_", function()
 	print(vim.inspect(lspbuf.list_workspace_folders()))
-end, opts)
-map("n", "st", lspbuf.type_definition, opts)
-map("n", "sv", lspbuf.rename, opts)
-map("n", "s<leader>a", lspbuf.code_action, opts)
-map("n", "s<leader>r", lspbuf.references, opts)
+end, { noremap = true, silent = true, desc = "List workspace folders" })
+map("n", "st", lspbuf.type_definition, { noremap = true, silent = true, desc = "Type definition" })
+map("n", "sv", lspbuf.rename, { noremap = true, silent = true, desc = "Rename" })
+map("n", "s<leader>a", lspbuf.code_action, { noremap = true, silent = true, desc = "Code actions" })
+map("n", "s<leader>r", lspbuf.references, { noremap = true, silent = true, desc = "See references" })
 
 lspconfig.pylsp.setup({
 	capabilities = lsp_defaults.capabilities,
