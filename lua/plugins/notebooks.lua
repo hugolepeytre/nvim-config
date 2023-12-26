5e8a6e063dbb393db4ba0b943760e1369f47e4ac
hugo lepeytre <hugo.lepeytre@cegedim-srh.com>
2023-12-26 16:57:10 +0100

    Added Molten show/hide output mappings

--- lua/plugins/notebooks.lua
+++ lua/plugins/notebooks.lua
@@ -10,26 +10,19 @@
 -- Can't display images
 -- Linting, formatting and most lsp functions unavailable inside ipynb (quarto-nvim and otter configs not finished)
 
-
+-- Molten setup
 local map = vim.keymap.set
-local otter = require("otter")
-map("n", "ssr", ":MoltenRestart!<CR>", { desc = "Restart current kernel and delete outputs" })
+map("n", "sskr", ":MoltenRestart!<CR>", { desc = "Restart current kernel and delete outputs" })
 map("n", "ssgg", ":MoltenGoto<CR>", { desc = "Go to first cell" })
 map("n", "ssgo", ":noautocmd MoltenEnterOutput<CR>", { desc = "Enter cell's output" })
 map("n", "ssj", ":MoltenNext<CR>", { desc = "Go to next cell" })
 map("n", "ssk", ":MoltenPrev<CR>", { desc = "Go to previous cell" })
 map("n", "ssd", ":MoltenDelete<CR>", { desc = "Delete current cell" })
 map("n", "ssel", ":MoltenEvaluateLine<CR>", { desc = "Evaluate current line" })
-map("n", "sser", ":MoltenReevaluateCell<CR>", { desc = "Reevaluate current cell" })
+map("n", "ssr", ":MoltenReevaluateCell<CR>", { desc = "Reevaluate current cell" })
+map("n", "ssos", ":MoltenShowOutput<CR>", { desc = "Show output" })
+map("n", "ssoh", ":MoltenHideOutput<CR>", { desc = "Hide output" })
 map("v", "sse", ":<C-u>MoltenEvaluateVisual<CR>gv", { desc = "Evaluate current line" })
-
-map("n", "ssa", otter.dev_setup, { desc = "Activate otter" })
-map("n", "ssgd", otter.ask_definition, { noremap = true, silent = true, desc = "Go to definition" })
-map("n", "sst", otter.ask_type_definition, { noremap = true, silent = true, desc = "Type definition" })
-map("n", "ssh", otter.ask_hover, { noremap = true, silent = true, desc = "Hover" })
-map("n", "ss<leader>r", otter.ask_references, { noremap = true, silent = true, desc = "See references" })
-map("n", "ssv", otter.ask_rename, { noremap = true, silent = true, desc = "Rename" })
-map("n", "ssf", otter.ask_format, { noremap = true, silent = true, desc = "Format" })
 
 -- Initialize a kernel with the same name as the venv
 map("n", "ssi", function()
@@ -41,6 +34,8 @@ map("n", "ssi", function()
         vim.cmd("MoltenInit python3")
     end
 end, { desc = "Initialize Molten kernel" })
+
+vim.g.molten_output_virt_lines = true
 -- After installing molten, run :UpdateRemotePlugins
 
 -- A project's venv needs to have ipykernel installed, then running :
@@ -49,6 +44,8 @@ end, { desc = "Initialize Molten kernel" })
 
 -- Then kernel has to be init before anything works
 
+
+-- Quarto and otter setup (todo)
 require('quarto').setup({
     lspFeatures = {
         languages = { 'python' },
@@ -71,3 +68,12 @@ require('quarto').setup({
         references = 'gr',
     }
 })
+
+local otter = require("otter")
+map("n", "ssa", otter.dev_setup, { desc = "Activate otter" })
+map("n", "ssgd", otter.ask_definition, { noremap = true, silent = true, desc = "Go to definition" })
+map("n", "sst", otter.ask_type_definition, { noremap = true, silent = true, desc = "Type definition" })
+map("n", "ssh", otter.ask_hover, { noremap = true, silent = true, desc = "Hover" })
+map("n", "ss<leader>r", otter.ask_references, { noremap = true, silent = true, desc = "See references" })
+map("n", "ssv", otter.ask_rename, { noremap = true, silent = true, desc = "Rename" })
+map("n", "ssf", otter.ask_format, { noremap = true, silent = true, desc = "Format" })
