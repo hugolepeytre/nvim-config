51c06849d759d27f074164f70c72a78cf61188d8
hugo lepeytre <hugo.lepeytre@cegedim-srh.com>
2024-01-03 17:10:49 +0100

    Started nightfox color tweak. Should be improved

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,3 +5,12 @@ require('nightfox').setup({
 })
 
 vim.cmd.colorscheme('nightfox')
+
+-- TODO choose correct colors
+vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#719cd6" })
+vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#63cdcf" })
+vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#81b29a" })
+vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#f4a261" })
+vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#f4a261" })
+vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#9d79d6" })
+vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#63cdcf" })
