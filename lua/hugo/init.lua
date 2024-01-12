c53702109293bf5dbb4c8297a11d478ff45b12a3
hugo lepeytre <hugo.lepeytre@cegedim-srh.com>
2024-01-12 15:41:52 +0100

    Second refactor checkpoint. Some plugins might have their setup function not called anymore

--- lua/hugo/init.lua
+++ lua/hugo/init.lua
@@ -1,11 +1,23 @@
+-- -- <leader> key (has to be registered before lazy in the config)
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
+
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--     vim.fn.system({
+--         "git",
+--         "clone",
+--         "--filter=blob:none",
+--         "https://github.com/folke/lazy.nvim.git",
+--         "--branch=stable", -- latest stable release
+--         lazypath,
+--     })
+-- end
+
+-- vim.opt.rtp:prepend(lazypath)
+-- require("lazy").setup("plugins")
+
 require("plugins.lazy")
-require("plugins.navigation")
-require("plugins.treesitter")
-require("plugins.lsp")
-require("plugins.autocomplete")
-require("plugins.autoformat")
-require("plugins.lint")
-require("plugins.notebooks")
 
 require("hugo.set")
 require("hugo.remap")
