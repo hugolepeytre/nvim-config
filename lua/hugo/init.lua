b74ffb3708c163ce2b43c59700a831445335b89a
hugo lepeytre <hugo.lepeytre@cegedim-srh.com>
2024-01-12 13:20:51 +0100

    Removed deleted file require

--- lua/hugo/init.lua
+++ lua/hugo/init.lua
@@ -8,7 +8,6 @@ require("plugins.autoformat")
 require("plugins.lint")
 require("plugins.statusbar")
 require("plugins.notebooks")
-require("plugins.test")
 
 require("hugo.set")
 require("hugo.remap")
