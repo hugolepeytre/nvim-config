a24342f267607ea6196c5fd8684b390484dfb541
hugo lepeytre <hugo.lepeytre@cegedim-srh.com>
2024-01-16 14:11:32 +0100

    Python poetry config default

--- init.lua
+++ init.lua
@@ -2,3 +2,26 @@
 -- TODO : Clean plugin config files (lsp, notebook, remaps, completion)
 
 require("hugo")
+
+-- [tool.poetry.dependencies]
+-- python = "^3.10"
+
+-- [tool.poetry.group.dev.dependencies]
+-- ruff = "0.1.9"
+-- mypy = "1.7.1"
+-- ipykernel = "6.27.1"
+-- jupytext = "1.16.0"
+
+-- [tool.mypy]
+-- check_untyped_defs = true
+
+-- [tool.ruff]
+-- target-version = "py310"
+
+-- [tool.ruff.lint]
+-- select = ["E", "D", "F", "W", "I", "PD", "PL", "NPY", "RUF", "B", "A", "C4", "DTZ", "DJ", "ISC", "PIE", "RET", "SIM", "ARG", "PTH"]
+-- fixable = ["I"]
+-- unfixable = []
+
+-- [tool.ruff.format]
+-- quote-style = "single"
