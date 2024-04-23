cdbb5a7f12f252542e8f8039bf5d1e6bf71d6cff
hugo lepeytre <hugo.lepeytre@cegedim-srh.com>
2024-04-23 10:42:28 +0200

    Better default color theme setting

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,17 +1,3 @@
-vim.cmd.colorscheme("nightfox")
+local colors = require("hugo.colors")
 
-vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#719cd6" })
-vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#63cdcf" })
-vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#81b29a" })
-vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#f4a261" })
-vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#9d79d6" })
-
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3c5372" })
-vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3c5372" })
-vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3c5372" })
-vim.api.nvim_set_hl(0, "Visual", { bg = "#3c5372" })
-
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+colors.set_light_theme()
