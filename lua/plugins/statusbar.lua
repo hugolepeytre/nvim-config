b0a8dfe3572eafcd5343ffe32e19b7c2fea8a08d
hugo lepeytre <hugo.lepeytre@cegedim-srh.com>
2024-04-24 09:19:40 +0200

    Adjusted status line theme

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -4,7 +4,7 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
 			options = {
-				theme = "nightfox",
+				theme = "dayfox",
 			},
 			sections = {
 				lualine_a = { "mode" },
